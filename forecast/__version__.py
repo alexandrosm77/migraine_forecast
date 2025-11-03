a644d2db62bed6659fd6fa93801875b878dbd140
Alexandros Mystridis <alexandrosm77@gmail.com>
2025-11-03 15:09:17 +0000

    Bump version to 0.1.5 for email localization fix

--- forecast/__version__.py
+++ forecast/__version__.py
@@ -9,7 +9,7 @@ Version follows Semantic Versioning (SemVer): MAJOR.MINOR.PATCH
 - PATCH: Backwards-compatible bug fixes
 """
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
 
 # Version metadata
